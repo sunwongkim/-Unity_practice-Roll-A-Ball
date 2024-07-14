daa154bbe9ac8e7c5cdc8dcdf091e0decc1ea4f3
sunwong Kim <sunwongkim0609@gmail.com>
2024-07-15 02:27:25 +0900

    물리 이벤트, UI

--- Assets/OtherBall.cs
+++ Assets/OtherBall.cs
@@ -4,13 +4,21 @@ public class OtherBall : MonoBehaviour
 {
     MeshRenderer myMesh;
     Material myMat;
+
     void Start()
     {
         myMesh = GetComponent<MeshRenderer>();
         myMat = myMesh.material;
     }
-    private void OnCollisionEnter(Collision other) {
-    // private void OnCollisionEnter(Collision collision) {
-        myMat.color = new Color(0, 0, 0);
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Cube"){
+            // myMat.color = new Color(0, 0, 0);
+            if (myMat.color == Color.red){
+                myMat.color = Color.blue;
+            }
+            else myMat.color = Color.red;
+        }
     }
 }
--- Assets/Physics.cs
+++ Assets/Physics.cs
@@ -8,17 +8,29 @@ public class Physics : MonoBehaviour
         myRigid = GetComponent<Rigidbody>();
     }
 
-    void FixedUpdate() {
+    void FixedUpdate()
+    {
         // W,A,S,D 이동
-        Vector3 vec =
-            new Vector3(Input.GetAxisRaw("Horizontal"),
-                                        0,
-                                        Input.GetAxisRaw("Vertical"));
+        float side = Input.GetAxisRaw("Horizontal");
+        float front = Input.GetAxisRaw("Vertical");
+        Vector3 vec = new Vector3(side, 0, front);
+
         myRigid.AddForce(vec, ForceMode.Impulse); // obj mass = 5
         
         // 점프 기본 예제
         // if(Input.GetKeyDown("Jump")) // Jump(space)를 인식못함.
         if(Input.GetKey(KeyCode.Space))
-            myRigid.AddForce(Vector3.up * 1, ForceMode.Impulse);
+            myRigid.AddForce(Vector3.up * 2, ForceMode.Impulse);
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.R)) // 위치 초기화
+            transform.position = new Vector3(0, 3, 0);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.name == "JumpZone")
+            myRigid.AddForce(Vector3.up, ForceMode.Impulse);
     }
 }
